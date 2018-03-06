abbfade8f359163db95f10eba2ea3b04bfd868b8
dkn22 <nguyendatkhac@gmail.com>
2018-03-06 22:53:13 +0000

    added new test

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,12 +1,13 @@
-from sklearn.datasets import make_classification
-from embedder.classification import Embedder
+from sklearn.datasets import make_regression
+from embedder.regression import Embedder
 from embedder.preprocessing import categorize, size_embeddings, encode_categorical
 import pandas as pd
+import numpy as np
 import pytest
 
 @pytest.fixture
 def test_data():
-    X, y = make_classification(n_samples=10000, n_features=20)
+    X, y = make_regression(n_samples=10000, n_features=20)
 
     X = pd.DataFrame(X).rename(columns={0: 'Categorical 1',
                                         10: 'Categorical 2'})
@@ -34,12 +35,13 @@ def test_fit_predict(test_data):
     preds = embedder.predict(X[:100])
 
     assert len(preds) == 100
-    assert all(preds <= 1) and all(preds >= 0)
+    assert not np.isinf(preds).any()
+    assert not np.isnan(preds).any()
 
 def test_transform(test_data):
     X, y = test_data
     cat_sz = categorize(X)
-    emb_sz = size_embeddings(cat_sz, max_dim=50)
+    emb_sz = size_embeddings(cat_sz)
     X, encoders = encode_categorical(X)
 
     embedder = Embedder(emb_sz)
@@ -50,16 +52,3 @@ def test_transform(test_data):
 
     assert transformed.shape == (10000, 18 + 50 + 50)
     assert embedded.shape == (10000, 18 + 50 + 50)
-
-
-
-
-
-
-
-
-
-
-
-
-
